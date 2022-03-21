5e0269ad5fb4ad4a581d37e27fb55ee9a0819e17
Sunil Kumar <sunil.kumar@thoughtworks.com>
2022-03-21 06:38:34 +0530

    use junit 5 annotation and methods in test

--- src/test/java/com/thoughtworks/cashregister/CashRegisterTest.java
+++ src/test/java/com/thoughtworks/cashregister/CashRegisterTest.java
@@ -1,13 +1,13 @@
 package com.thoughtworks.cashregister;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CashRegisterTest {
 
     @Test
     public void dummyTest() {
-        assertEquals("Integer One is equal to Integer 1", 1, 1);
+        assertEquals(1, 1);
     }
 }
